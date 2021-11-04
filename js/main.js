de18161087ad588525ffee0f74cfc9b5260b5654
XurshidJuraev <sirlibloger37@gmail.com>
2021-11-04 20:12:30 +0500

    Firs commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var  howMuchMoneyDoYouThinkHeNeeds = prompt('Alisherga qancha pul kerak deb oylaysiz') ;
+alert(howMuchMoneyDoYouThinkHeNeeds);
+console.log(howMuchMoneyDoYouThinkHeNeeds);
+
+var  howMuchMoneyDoYouThinkHeNeeds = prompt('Samalyot bileti uchun qancha kerak deb oylaysiz') ;
+alert(howMuchMoneyDoYouThinkHeNeeds);
+console.log(howMuchMoneyDoYouThinkHeNeeds);
+
+var  howMuchMoneyDoYouThinkHeNeeds = prompt('mehmonxona uchun qancha kerak deb oylaysiz') ;
+alert(howMuchMoneyDoYouThinkHeNeeds);
+console.log(howMuchMoneyDoYouThinkHeNeeds);
+
+var  howMuchMoneyDoYouThinkHeNeeds = prompt('Muzey va kongilochar joylar uchun qancha kerak deb oylaysiz') ;
+alert(howMuchMoneyDoYouThinkHeNeeds);
+console.log(howMuchMoneyDoYouThinkHeNeeds);
+
+var UmumiyPul = 870;
+
+if ((UmumiyPul >= "8317488.6") == (UmumiyPul <= "8317488.6")) {
+    console.log("Sizning pulingiz hamma harajatlarga yetdi va hisobingizda 0so'm qoldi! “Oq yo’l, Alisher!”");
+}else {
+    console.log("Pulingiz yetmadi! “Alisher, ozgina sabr qilish kerak bo’lar ekan.”");
+};
